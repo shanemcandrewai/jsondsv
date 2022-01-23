3b37334844e84cb0dd5e751cebd666f9f1692b2b
shane <s_mcandrew@yahoo.com>
2022-01-23 15:46:37 +0100

    add debug

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
+const debug = require('debug')('app');
 
 fs.readFile('jsonNest.json', 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    // console.error(err);
+    debug(err);
     return;
   }
   // parse JSON string to JSON object
   const jsonNest = JSON.parse(data);
   // jsonNest.forEach(db => {
-  console.log(jsonNest);
+  // console.log(jsonNest);
+  debug(jsonNest);
   // });
 });
