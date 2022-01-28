4a838388b54f58dc5cce56957b1fb367213fde1a
shane <s_mcandrew@yahoo.com>
2022-01-28 09:39:05 +0100

    add tab2nest.json tests

--- tab2nest.js
+++ tab2nest.js
@@ -12,15 +12,17 @@ for (const [key, value] of Object.entries(jno)) {
   console.log(`${key}: ${value}`);
 }
 
+var jn = {};
 var itout = (jno) => {
   for (const property in jno) {
     console.log(`${property}: ${jno[property]}`);
     if (typeof jno[property] === 'object') {
       itout(jno[property])
     }
+    jn[property]['a'] = 1;
   }
 }
-
 itout(jno);
+jn;
 
 itout(jno['row-0']);
