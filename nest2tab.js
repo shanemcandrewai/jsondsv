8587da01b3395585e70ca839d38baa948f26da41
shane <s_mcandrew@yahoo.com>
2022-01-29 21:27:20 +0100

    dev nest2tab

--- nest2tab.js
+++ nest2tab.js
@@ -2,6 +2,7 @@
 // DEBUG=app node nest2tab.js
 
 const debug = require('debug')('app');
+
 // const fs = require('fs');
 // const data = fs.readFileSync('jsonNest.json', 'utf8');
 // const jstf = JSON.parse(data);
@@ -27,14 +28,23 @@ const jsn = {
 };
 
 const jst = {};
-const n2t = (jso, path) => {
+const n2t = (jso, key1, path) => {
   Object.entries(jso).forEach(([key, value]) => {
-    if (typeof jso[key] === 'object') {
-      jst[`${path}.${key}`] = {};
-      debug(`${path}.${key}`);
-      n2t(value, `${path}.${key}`);
+    if (key1 !== undefined) {
+      path = key;
     } else {
-      jst[key] = value;
+      path += '.';
+      path += key;
+    }
+
+    if (typeof jso[key] === 'object') {
+      if (key1 !== undefined) {
+        n2t(value, key1, path);
+      } else {
+        n2t(value, key, path);
+      }
+    } else {
+      jst[path] = jso[key];
     }
   });
 };
