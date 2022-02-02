f5c770407f1c31bc34e8f6df92360fa22c60615c
shane <s_mcandrew@yahoo.com>
2022-02-02 21:16:19 +0100

    add t2n stub

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
 const fs = require('fs');
 const debug = require('debug')('app');
 
+const n2t = (ob, path, jst) => {
+  const jstt = (jst !== undefined) ? jst : {};
+  Object.entries(ob).forEach(([key, value]) => {
+    let newpath;
+    if (Array.isArray(ob)) {
+      newpath = (path !== undefined) ? `${path}[${key}]` : `[${key}]`;
+    } else {
+      newpath = (path !== undefined) ? `${path}.${key}` : key;
+    }
+    if (typeof value === 'object') {
+      Object.assign(jstt, n2t(value, newpath, jstt));
+    } else {
+      const columns = {};
+      const rowkey = newpath.split('.')[0];
+      columns[newpath.split(/\.(.+)/)[1]] = value;
+      if (jstt[rowkey] === undefined) {
+        jstt[rowkey] = columns;
+      } else {
+        Object.assign(jstt[rowkey], columns);
+      }
+    }
+  });
+  fs.writeFile('jst.json', JSON.stringify(jstt), (errw) => {
+    if (errw) { debug(errw); }
+  });
+};
+
+const t2n = (ob, path, jsn) => {
+  const jsnn = (jsn !== undefined) ? jsn : { xxx: 2 };
+  fs.writeFile('jsn.json', JSON.stringify(jsnn), (errw) => {
+    if (errw) { debug(errw); }
+  });
+};
+
 fs.readFile('jsonNest.json', 'utf8', (err, data) => {
   if (err) {
     debug(err);
     return;
   }
 
-  const n2t = (ob, path, jst) => {
-    const jstt = (jst !== undefined) ? jst : {};
-    Object.entries(ob).forEach(([key, value]) => {
-      let newpath;
-      if (Array.isArray(ob)) {
-        newpath = (path !== undefined) ? `${path}[${key}]` : `[${key}]`;
-      } else {
-        newpath = (path !== undefined) ? `${path}.${key}` : key;
-      }
-      if (typeof value === 'object') {
-        Object.assign(jstt, n2t(value, newpath, jstt));
-      } else {
-        const columns = {};
-        const rowkey = newpath.split('.')[0];
-        columns[newpath.split(/\.(.+)/)[1]] = value;
-        if (jstt[rowkey] === undefined) {
-          jstt[rowkey] = columns;
-        } else {
-          Object.assign(jstt[rowkey], columns);
-        }
-      }
-    });
-    fs.writeFile('jst.json', JSON.stringify(jstt), (errw) => {
-      if (errw) { debug(errw); }
-    });
-  };
-
   n2t(JSON.parse(data));
+});
+
+fs.readFile('jst.json', 'utf8', (err, data) => {
+  if (err) {
+    debug(err);
+    return;
+  }
+
+  t2n(JSON.parse(data));
 });
