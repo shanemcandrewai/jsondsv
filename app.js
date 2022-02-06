213b5c30ebab42bc9a74a62cde8d8e0f8125042b
shane <s_mcandrew@yahoo.com>
2022-02-06 11:43:02 +0100

    jsonToTab WIP

--- app.js
+++ app.js
@@ -1,58 +1,79 @@
 const fs = require('fs');
 const debug = require('debug')('app');
 const set = require('lodash/set');
+const isEqual = require('lodash/isEqual');
 
-const t2n = (ob) => {
-  const jsn = {};
-  Object.entries(ob).forEach(([rowkey, columns]) => {
-    Object.entries(columns).forEach(([path, value]) => {
-      set(jsn, `${rowkey}.${path}`, value);
-    });
-  });
-  fs.writeFile('jsn.json', JSON.stringify(jsn), (err) => {
-    if (err) { debug(err); }
-  });
-};
-
-const n2t = (ob, path, jst) => {
-  let tsv = (jst === undefined) ? '' : jst;
-  Object.entries(ob).forEach(([key, value]) => {
+const jsonToTab = (obj, path, tsv) => {
+  let tsvBuild = (tsv === undefined) ? '' : tsv;
+  Object.entries(obj).forEach(([key, value]) => {
     let newpath;
-    if (Array.isArray(ob)) {
-      newpath = (path !== undefined) ? `${path}[${key}]` : `[${key}]`;
+    if (Array.isArray(obj)) {
+      newpath = (path === undefined) ? `[${key}]` : `${path}[${key}]`;
     } else {
-      newpath = (path !== undefined) ? `${path}.${key}` : key;
+      newpath = (path === undefined) ? key : `${path}.${key}`;
     }
     if (typeof value === 'object') {
-      tsv = n2t(value, newpath, tsv);
+      tsvBuild = jsonToTab(value, newpath, tsvBuild);
     } else {
+      // Update column headers if necessary
       const columnLabel = newpath.split(/\.(.+)/)[1];
-      if (!tsv.includes(columnLabel)) {
-        if (tsv.slice(-1) !== ' ' && tsv.length) {
-          tsv += '\t';
+      const indColumnLabel = tsvBuild.indexOf(columnLabel);
+      const indEndHeader = tsvBuild.indexOf('\n');
+      if (indColumnLabel === -1) {
+        if (tsvBuild.length) {
+          tsvBuild = `${tsvBuild.slice(0, indEndHeader)}\t${columnLabel}${tsvBuild.slice(indEndHeader)}`;
+        } else {
+          tsvBuild = `${columnLabel}\n`;
         }
-        tsv += columnLabel;
       }
-      // const columns = {};
-      // const rowkey = newpath.split('.')[0];
-      // columns[newpath.split(/\.(.+)/)[1]] = value;
-      // if (tsv[rowkey] === undefined) {
-      // tsv[rowkey] = columns;
-      // } else {
-      // Object.assign(tsv[rowkey], columns);
-      // }
+      // Append separators for empty columns and value
+      const lastNewline = tsvBuild.lastIndexOf('\n');
+      const numberCols = ((tsvBuild.slice(0, indColumnLabel).match(/\t/g) || []).length);
+      for (let i = 0; i < numberCols; i += 1) {
+
+      }
     }
   });
-  return tsv;
+  return tsvBuild;
 };
 
-fs.readFile('jsonNest.json', 'utf8', (err, data) => {
-  if (err) {
-    debug(err);
-    return;
-  }
-  const obj = JSON.parse(data);
-  fs.writeFile('jsn.tsv', n2t(obj), (errw) => {
-    if (errw) { debug(errw); }
+const tabToJson = (tsvBuild) => {
+  const jsonObj = {};
+  Object.entries(tsvBuild).forEach(([rowkey, columns]) => {
+    Object.entries(columns).forEach(([path, value]) => {
+      set(jsonObj, `${rowkey}.${path}`, value);
+    });
   });
-});
+  return jsonObj;
+};
+
+switch (process.argv[2].slice(-5)) {
+  case '.json':
+    fs.readFile(process.argv[2], 'utf8', (err, data) => {
+      if (err) {
+        debug(err);
+        return;
+      }
+      const obj = JSON.parse(data);
+      const fileOut = `${process.argv[2].split(/\.(.+)/)[0]}.tsv`;
+      fs.writeFile(fileOut, jsonToTab(obj), (errw) => {
+        if (errw) { debug(errw); }
+      });
+    });
+    break;
+
+  default:
+    fs.readFile('nested.tsv', 'utf8', (errt, tsvBuild) => {
+      if (errt) {
+        debug(errt);
+        return;
+      }
+      fs.readFile('nested.json', 'utf8', (errj, jsonObj) => {
+        if (errj) {
+          debug(errj);
+          return;
+        }
+        debug('matched', isEqual(tabToJson(tsvBuild), JSON.parse(jsonObj)));
+      });
+    });
+}
