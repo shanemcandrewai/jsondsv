02cf9b7624df67fa5cfbc94fc13d5de1c56e0e64
shane <s_mcandrew@yahoo.com>
2022-02-08 07:31:36 +0100

    rename jsondsv

--- jsondsv.js
+++ jsondsv.js
@@ -1,10 +1,133 @@
-// JSONtab library
-// A tool to reversibly convert between an arbitrarily deep nested object and a
-// tabular format, TSV by default.
+// JSONDSV library
+// ===============
+// A tool to reversibly convert between an arbitrarily deep nested array
+// and a configurable delimiter separated values (DSV) format such as tab (TSV),
+// comma (CSV), pipe, etc. Reversibility is achieved by populating the
+// header row of the DSV with paths from the array.
+
+// See below for an example of a nested array (testArray), its equivalent DSV (testTSV)
+// and a simple test runner. With node.js, the tests can be executed with
+// DEBUG=app node jsondsv.js
 
 const debug = require('debug')('app');
 const set = require('lodash/set');
 const isEqual = require('lodash/isEqual');
+
+// Helper function calcSeparators
+// returns a string of column separators representing
+// empty fields in the last row of the DSV based on the position of
+// columnLabel within the column header
+const calcSeparators = (columnLabel, dsv, colSep, lineSep) => {
+// dsv : recursively built DSV
+// colSep : Column separator, default tab
+// lineSep : Line separator, default newline
+  const colSepChar = (colSep === undefined) ? '\t' : colSep;
+  const lineSepChar = (lineSep === undefined) ? '\n' : lineSep;
+  const colSepRegex = new RegExp(colSepChar, 'g');
+  const lineSepRegex = new RegExp(`${lineSepChar}(.+)$`, 'g');
+  const colHeader = dsv.split(lineSepChar)[0];
+  const indColumnLabel = colHeader.indexOf(columnLabel);
+  const labelNumber = (colHeader.slice(0, indColumnLabel).match(colSepRegex) || []).length;
+  const currentRow = dsv.split(lineSepRegex)[1];
+  const currColNumber = (currentRow.match(colSepRegex) || []).length;
+  const addSeparators = labelNumber - currColNumber;
+
+  let separators = '';
+  for (let i = 0; i < addSeparators; i += 1) {
+    separators = `${separators}${colSepChar}`;
+  }
+  return separators;
+};
+
+// TODO fix
+// Function arrayToDSV returns a DSV from an arbitrary nested object JSONObject.
+// pathElement : recursively built path for each value, used to populate column headers
+// this preserves the object structure in order to reconstruct it later,
+// see function tableToJSON
+const arrayToDSV = (JSONObject, pathElement, sepValueElement, colSep, lineSep) => {
+// sepValueElement : recursively built separated value table
+// colSep : Column separator, default tab
+// lineSep : Line separator, default newline
+  let dsv = (sepValueElement === undefined) ? '' : sepValueElement;
+  const colSepChar = (colSep === undefined) ? '\t' : colSep;
+  const lineSepChar = (lineSep === undefined) ? '\n' : lineSep;
+  Object.entries(JSONObject).forEach(([key, value]) => {
+    let pathBuild;
+    if (Array.isArray(JSONObject)) {
+      pathBuild = (pathElement === undefined) ? `[${key}]` : `${pathElement}[${key}]`;
+    } else {
+      pathBuild = (pathElement === undefined) ? key : `${pathElement}.${key}`;
+    }
+
+    if (typeof value === 'object') {
+      dsv = arrayToDSV(value, pathBuild, dsv);
+    } else {
+      const columnLabel = pathBuild.split(/\.(.+)/)[1];
+      const indEndHeader = dsv.indexOf(lineSepChar);
+      const colHeader = dsv.slice(0, indEndHeader);
+      const indColumnLabel = colHeader.indexOf(columnLabel);
+
+      if (indColumnLabel === -1) {
+        if (dsv.length) {
+          // Add new column
+          dsv = `${colHeader}${colSepChar}${columnLabel}${dsv.slice(indEndHeader)}`;
+          const separators = calcSeparators(columnLabel, dsv, colSepChar);
+          dsv = `${dsv}${separators}${value}`;
+        } else {
+          // Add first column
+          dsv = `${columnLabel}${lineSepChar}`;
+          dsv = `${dsv}${value}`;
+        }
+      } else if (dsv.slice(-1) === lineSepChar) {
+        // Add first value in new row
+        dsv = `${dsv}${value}`;
+      } else {
+        // Calculate column position and add subsequent value in row
+        const separators = calcSeparators(columnLabel, dsv);
+        dsv = `${dsv}${separators}${value}`;
+      }
+    }
+  });
+  if (pathElement !== undefined && !pathElement.includes('.')) {
+    // End of row
+    dsv = `${dsv}${lineSepChar}`;
+  }
+  return dsv;
+};
+
+const tableToJSON = (dsv, colSep, lineSep) => {
+// Return an arbitrary nested object from a separate value table dsv
+// The table column header contains paths which are used to reconstruct
+// the object.
+// colSep : Column separator, default tab
+// lineSep : Line separator, default newline
+  const colSepChar = (colSep === undefined) ? '\t' : colSep;
+  const lineSepChar = (lineSep === undefined) ? '\n' : lineSep;
+  const jsonArr = [];
+  let rowObj = {};
+  const rows = dsv.split(lineSepChar);
+  let paths;
+  rows.forEach((row, rowInd) => {
+    if (!rowInd) {
+      paths = row.split(colSepChar);
+    } else {
+      const values = row.split(colSepChar);
+      values.forEach((value, columnInd) => {
+        const pathElement = `${paths[columnInd]}`;
+        if (value) {
+          // remove quotes from value if possible
+          const valueNum = value * 1;
+          set(rowObj, pathElement, Number.isNaN(valueNum) ? value : valueNum);
+        }
+      });
+      if (Object.keys(rowObj).length) {
+        jsonArr.push(rowObj);
+        rowObj = {};
+      }
+    }
+  });
+  return jsonArr;
+};
 
 // test data
 const testJSON = [
@@ -40,121 +163,8 @@ const testTSV = 'row\trec-0.date\trec-0.tags[0]\trec-1.date\trec-1.url\trec-0.ur
 + '0\t20220121\tval-0\t20220116\thttps://example.com/a\n'
 + '1\t20220116\t\t\t\thttps://example.com/b\tval-0\tval-1\n';
 
-const calcSeparators = (columnLabel, sepValueString, colSep, lineSep) => {
-// Return a string of column separators representing empty fields in the
-// current row based on the position of columnLabel within the column header
-// sepValueString : recursively built separated value table
-// colSep : Column separator, default tab
-// lineSep : Line separator, default newline
-  const colSepChar = (colSep === undefined) ? '\t' : colSep;
-  const lineSepChar = (lineSep === undefined) ? '\n' : lineSep;
-  const colSepRegex = new RegExp(colSepChar, 'g');
-  const lineSepRegex = new RegExp(`${lineSepChar}(.+)$`, 'g');
-  const colHeader = sepValueString.split(lineSepChar)[0];
-  const indColumnLabel = colHeader.indexOf(columnLabel);
-  const labelNumber = (colHeader.slice(0, indColumnLabel).match(colSepRegex) || []).length;
-  const currentRow = sepValueString.split(lineSepRegex)[1];
-  const currColNumber = (currentRow.match(colSepRegex) || []).length;
-  const addSeparators = labelNumber - currColNumber;
-
-  let separators = '';
-  for (let i = 0; i < addSeparators; i += 1) {
-    separators = `${separators}${colSepChar}`;
-  }
-  return separators;
-};
-
-const JSONToTable = (JSONObject, pathElement, sepValueElement, colSep, lineSep) => {
-// Return a separated value table from an arbitrary nested object JSONObject.
-// pathElement : recursively built path for each value, used to populate column headers
-// this preserves the object structure in order to reconstruct it later,
-// see function tableToJSON
-// sepValueElement : recursively built separated value table
-// colSep : Column separator, default tab
-// lineSep : Line separator, default newline
-  let sepValueString = (sepValueElement === undefined) ? '' : sepValueElement;
-  const colSepChar = (colSep === undefined) ? '\t' : colSep;
-  const lineSepChar = (lineSep === undefined) ? '\n' : lineSep;
-  Object.entries(JSONObject).forEach(([key, value]) => {
-    let pathBuild;
-    if (Array.isArray(JSONObject)) {
-      pathBuild = (pathElement === undefined) ? `[${key}]` : `${pathElement}[${key}]`;
-    } else {
-      pathBuild = (pathElement === undefined) ? key : `${pathElement}.${key}`;
-    }
-
-    if (typeof value === 'object') {
-      sepValueString = JSONToTable(value, pathBuild, sepValueString);
-    } else {
-      const columnLabel = pathBuild.split(/\.(.+)/)[1];
-      const indEndHeader = sepValueString.indexOf(lineSepChar);
-      const colHeader = sepValueString.slice(0, indEndHeader);
-      const indColumnLabel = colHeader.indexOf(columnLabel);
-
-      if (indColumnLabel === -1) {
-        if (sepValueString.length) {
-          // Add new column
-          sepValueString = `${colHeader}${colSepChar}${columnLabel}${sepValueString.slice(indEndHeader)}`;
-          const separators = calcSeparators(columnLabel, sepValueString, colSepChar);
-          sepValueString = `${sepValueString}${separators}${value}`;
-        } else {
-          // Add first column
-          sepValueString = `${columnLabel}${lineSepChar}`;
-          sepValueString = `${sepValueString}${value}`;
-        }
-      } else if (sepValueString.slice(-1) === lineSepChar) {
-        // Add first value in new row
-        sepValueString = `${sepValueString}${value}`;
-      } else {
-        // Calculate column position and add subsequent value in row
-        const separators = calcSeparators(columnLabel, sepValueString);
-        sepValueString = `${sepValueString}${separators}${value}`;
-      }
-    }
-  });
-  if (pathElement !== undefined && !pathElement.includes('.')) {
-    // End of row
-    sepValueString = `${sepValueString}${lineSepChar}`;
-  }
-  return sepValueString;
-};
-
-const tableToJSON = (sepValueString, colSep, lineSep) => {
-// Return an arbitrary nested object from a separate value table sepValueString
-// The table column header contains paths which are used to reconstruct
-// the object.
-// colSep : Column separator, default tab
-// lineSep : Line separator, default newline
-  const colSepChar = (colSep === undefined) ? '\t' : colSep;
-  const lineSepChar = (lineSep === undefined) ? '\n' : lineSep;
-  const jsonArr = [];
-  let rowObj = {};
-  const rows = sepValueString.split(lineSepChar);
-  let paths;
-  rows.forEach((row, rowInd) => {
-    if (!rowInd) {
-      paths = row.split(colSepChar);
-    } else {
-      const values = row.split(colSepChar);
-      values.forEach((value, columnInd) => {
-        const pathElement = `${paths[columnInd]}`;
-        if (value) {
-          // remove quotes from value if possible
-          const valueNum = value * 1;
-          set(rowObj, pathElement, Number.isNaN(valueNum) ? value : valueNum);
-        }
-      });
-      if (Object.keys(rowObj).length) {
-        jsonArr.push(rowObj);
-        rowObj = {};
-      }
-    }
-  });
-  return jsonArr;
-};
-
 // run tests
-const sepValueString = JSONToTable(testJSON);
-debug('matched test TSV', sepValueString === testTSV);
+const dsv = arrayToDSV(testJSON);
+debug('matched test TSV', dsv === testTSV);
 debug('matched test JSON', isEqual(tableToJSON(testTSV), testJSON));
-debug('matched reverse JSON', isEqual(tableToJSON(sepValueString), testJSON));
+debug('matched reverse JSON', isEqual(tableToJSON(dsv), testJSON));
